77ca5b2d32c8fdc344a7359c210d4cdbb90c5227
imohtesham <ibraahimm92@gmail.com>
2022-11-02 15:24:48 +0530

    Ability to create board

---
+++ TicTacToe.java
@@ -0,0 +1,19 @@
+public class TicTacToe {
+
+    static char[] board = new char[10];
+
+    static void createBoard(){
+        for(int index = 0 ; index<10 ; index++) {
+            board[index] = ' ';
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
+        createBoard();
+
+
+    }
+
+}
