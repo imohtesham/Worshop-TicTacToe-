1e6e4407d7405a4c4dd4360d31ff3779d4dcce8a
imohtesham <ibraahimm92@gmail.com>
2022-11-02 15:48:47 +0530

    Ability to create toss for players

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,7 +4,7 @@ public class TicTacToe {
 
     static char[] board = new char[10];
     static char player,computer;
-    public static Scanner scanner = new Scanner(System.in);
+    public static Scanner scannerObject = new Scanner(System.in);
     private static int playLocation;
 
     static void createBoard(){
@@ -14,7 +14,8 @@ public class TicTacToe {
     }
     static void getPlayerChoice() {
         System.out.print("select X or O : ");
-        player = Character.toUpperCase(scanner.next().charAt(0));
+
+        player = Character.toUpperCase(scannerObject.next().charAt(0));
 
         if (player == 'X')
             computer='O';
@@ -33,7 +34,8 @@ public class TicTacToe {
     }
     public static void userMove() {
         System.out.println("Enter Location 1-9 to Make Move");
-        playLocation = scanner.nextInt();
+        playLocation = scannerObject.nextInt();
+        scannerObject.nextLine();
         if (isEmpty() && playLocation < 10 && playLocation > 0) {
             board[playLocation] = player;
             showBoard();
@@ -50,6 +52,19 @@ public class TicTacToe {
         }
     }
 
+    public static void checkToss() {
+
+        double tossResult = Math.floor(Math.random() * 10) % 2;
+        System.out.println("\nChoose 1 for Heads or 2 for Tails");
+        int coinSelect = scannerObject.nextInt();
+        if (coinSelect == tossResult) {
+            System.out.println("\nPlayer Won The Toss! Player Starts");
+        } else {
+            System.out.println("\nComputer Won The Toss! Computer Starts");
+        }
+    }
+
+
     public static void main(String[] args) {
 
         System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
@@ -57,6 +72,7 @@ public class TicTacToe {
         getPlayerChoice();
         showBoard();
         userMove();
+        checkToss();
 
 
     }
